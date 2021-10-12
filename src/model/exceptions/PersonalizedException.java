542f0e1c43c77b3b446cc2126d8fcf80d24af441
Vanderson Tavares da Silva <vandersontavares32@gmail.com>
2021-10-12 20:24:11 -0300

    sem interface

---
+++ src/model/exceptions/PersonalizedException.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+
+//Não Usei
+public class PersonalizedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public PersonalizedException(String msg) {
+        super(msg);
+    }
+}
